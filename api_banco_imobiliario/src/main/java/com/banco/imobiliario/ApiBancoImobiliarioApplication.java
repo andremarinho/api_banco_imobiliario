68c0a054f1cd904f04f84651dd8e219379cc88d1
andremarinho <andre.bmarinho@hotmail.com>
2022-09-30 06:29:20 -0300

    Arquivos faltantes

---
+++ api_banco_imobiliario/src/main/java/com/banco/imobiliario/ApiBancoImobiliarioApplication.java
@@ -0,0 +1,13 @@
+package com.banco.imobiliario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoImobiliarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBancoImobiliarioApplication.class, args);
+    }
+
+}
---
+++ api_banco_imobiliario/src/test/java/com/banco/imobiliario/ApiBancoImobiliarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.imobiliario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoImobiliarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
